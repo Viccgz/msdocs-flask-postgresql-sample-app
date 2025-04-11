539a1079371514e05a0e856947ccaa2b4e88d305
Viccgz <147501719+Viccgz@users.noreply.github.com>
2025-04-11 07:14:30 +0000

    comentario

--- azureproject/production.py
+++ azureproject/production.py
@@ -1,5 +1,5 @@
 import os
-
+#hola
 DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
      dbuser=os.getenv('AZURE_POSTGRESQL_USER'),
      dbpass=os.getenv('AZURE_POSTGRESQL_PASSWORD'),
